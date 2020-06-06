0e38dee9f18a1b75fe3bf612cbdf5aa6a3471666
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-06-06 12:35:14 +0200

    Logging in Multiple Users

--- abelbok/routes/web.php
+++ abelbok/routes/web.php
@@ -22,4 +22,8 @@ Route::resource('items', 'ItemsController');
 
 Route::get('/dashboard', 'DashboardController@index');
 
-Route::get('/admin', 'AdminController@index');
+Route::prefix('admin')->group(function() {
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
